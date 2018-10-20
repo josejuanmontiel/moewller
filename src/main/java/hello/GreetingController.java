01eb6252e74344fe2dea7c3acab2e0d8b1bb54c1
Jose Juan Montiel <josejuan.montiel@gmail.com>
2018-10-20 08:08:33 +0200

    Add controller to show customers

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,12 +1,19 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    @Autowired
+    private CustomerRepository cRepository;
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -16,4 +23,13 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+
+    @RequestMapping("/customers")
+    public List<Customer> findAll() {
+        List<Customer> target = new ArrayList<>();
+        cRepository.findAll().forEach(target::add);
+        return target;
+    }
+    
+
 }
